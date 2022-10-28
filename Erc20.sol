dbf1f5850fce98e067c7187048abeb75f96074fd
Pothina Praveen <86554103+praveenrider784@users.noreply.github.com>
2022-10-28 14:35:51 +0530

    Update Erc20.sol

--- Erc20.sol
+++ Erc20.sol
@@ -1,61 +1,46 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
-
-contract VOHCoinERC20 {
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-
-    string public constant name = "VOH Coin";
-    string public constant symbol = "VOHN";
+contract Praveen{
+    event _transfer(address from,address to,uint tokens);
+    event approval(address tokenowner,address spender,uint tokens );
+    
+    string public constant name = "Praveen";
+    string public constant symbol = "PN";
     uint8 public constant decimals = 18;
-
-    mapping(address => uint256) balances;
-
-    mapping(address => mapping (address => uint256)) allowed;
-
-    uint256 totalSupply_;
-
-    constructor(uint256 total) {
-      totalSupply_ = total;
-      balances[msg.sender] = totalSupply_;
+    mapping(address=>uint)balances;
+    mapping(address=>mapping(address=>uint)) allowed;
+    uint256 _totalsupply;
+    constructor(uint256 total) public{
+      _totalsupply=total;
+      balances[msg.sender]=_totalsupply;
     }
-
-    function totalSupply() public view returns (uint256) {
-      return totalSupply_;
+   function totalsupply() public view returns(uint256){
+       return _totalsupply;
+   }
+    function balnceof(address tokenowner) public view returns(uint256){
+        return balances[tokenowner];
     }
-
-    function balanceOf(address tokenOwner) public view returns (uint) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address receiver, uint numTokens) public returns (bool) {
+      function transfer(address receiver, uint numTokens) public  {
              
         require(numTokens <= balances[msg.sender]);
         balances[msg.sender] -= numTokens;
         balances[receiver] += numTokens;
-        emit Transfer(msg.sender, receiver, numTokens);
-        return true;
+        emit _transfer(msg.sender, receiver, numTokens);
     }
-
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
+    function approve(address delegate,uint tokens) public{
+        allowed[msg.sender][delegate]=tokens;
+        emit approval(msg.sender,delegate,tokens);
     }
-
-    function allowance(address owner, address delegate) public view returns (uint) {
+    function allowance(address owner,address delegate) public view returns(uint){
         return allowed[owner][delegate];
     }
-
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-
-        balances[owner] -= numTokens;
-        allowed[owner][msg.sender] -= numTokens;
-        balances[buyer] += numTokens;
-        emit Transfer(owner, buyer, numTokens);
-        return true;
+    function transferfrom(address owner,address buyer,uint tokens) public{
+        require(tokens<=balances[owner]);
+        require(tokens<=allowed[owner][msg.sender]);
+        balances[owner]-=tokens;
+        allowed[owner][msg.sender]-=tokens;
+        balances[buyer]+=tokens;
+         emit _transfer(owner, buyer, tokens);
     }
+
 }
